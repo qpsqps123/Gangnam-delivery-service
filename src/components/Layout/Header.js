95e496ce22a7128ec77b6ff8fda5be15b4f07970
Jake Jeong <qpsqps321@gmail.com>
2023-09-06 19:24:57 +0900

    [feat] 헤더 작업: h1 태그를 a11y-hidden 클래스로 숨기고, 그 자리에 Gangnam 로고 이미지 추가.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,20 @@
 import HeaderCartButton from "./HeaderCartButton";
+
+import classes from "./Header.module.css";
+
 import mealsImage from "../../assets/meals-image.jpg";
+import gangnamLogo from "../../assets/gangnam-logo.png";
 
 const Header = () => {
   return (
     <>
-      <header>
-        <h1>GANGNAM</h1>
+      <header className={classes.header}>
+        <h1 className="a11y-hidden">Perth Gangnam restaurant</h1>
+        <img src={gangnamLogo} alt="Gangnam restaurant logo." />
         <HeaderCartButton />
       </header>
       <div>
-        <img src={mealsImage} alt="A table full of delicious Korean food!" />
+        <img src={mealsImage} alt="A table full of delicious Korean food." />
       </div>
     </>
   );
