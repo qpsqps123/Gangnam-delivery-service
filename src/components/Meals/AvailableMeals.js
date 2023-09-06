096228c5c89ec1da2414df84d71575605c91cde4
Jake Jeong <qpsqps321@gmail.com>
2023-09-06 20:30:15 +0900

    [feat] 주 콘텐츠 작업: 기본적인 마크업 추가.

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,13 @@
+import Card from "../UI/Card";
+
+const AvailableMeals = () => {
+  return (
+    <section>
+      <Card>
+        <ul>Meals List</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
