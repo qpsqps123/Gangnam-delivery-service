526f6c3861e3e767a57ac916f978e3f6621c82b0
Jake Jeong <qpsqps321@gmail.com>
2023-09-06 21:44:04 +0900

    [modify] 주 콘텐츠 작업: DUMMY_MENU에 한글 이름 추가.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,26 +5,26 @@ const AvailableMeals = () => {
   const DUMMY_MENU = [
     {
       id: "meal1",
-      name: "Bibimbap",
+      name: "Bibimbap 비빔밥",
       description: 'Korean style "Mixed Rice"',
       price: 23,
     },
     {
       id: "meal2",
-      name: "Bulgogi with Rice",
+      name: "Bulgogi with Rice 불고기",
       description:
         "Stir fried sliced meat, marinated in our special sauce with assroted vegetables",
       price: 23,
     },
     {
       id: "meal3",
-      name: "Japchae",
+      name: "Japchae 잡채",
       description: "Pan tossed sweet potato noodles",
       price: 23,
     },
     {
       id: "meal4",
-      name: "Jambong",
+      name: "Jambong 해물 짬뽕",
       description:
         "Korean spicy seafood noodle soup served with prawns, squid and assorted vegetables.",
       price: 23,
