4f371b6df25c3e00dc67f47430207d986a88fcec
Jake Jeong <qpsqps321@gmail.com>
2023-09-06 20:02:23 +0900

    [feat] 헤더 작업: HeaderCartButton 컴포넌트 스타일링.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,15 @@
 import CartIcon from "../Cart/CartIcon";
 
+import classes from "./HeaderCartButton.module.css";
+
 const HeaderCartButton = () => {
   return (
-    <button>
-      <span>
+    <button className={`${classes.button} ${classes.bump}`}>
+      <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span></span>
+      <span className={classes.badge}>0</span>
     </button>
   );
 };
