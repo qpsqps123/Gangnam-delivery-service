02bd7f053543e0dac99e0c0b66b16894f6f5a90f
Jake Jeong <qpsqps321@gmail.com>
2023-09-06 22:00:18 +0900

    [feat] 주 콘텐츠 작업: AvailableMeals 컴포넌트 스타일링

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,8 @@
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
+import classes from "./AvailableMeals.module.css";
+
 const AvailableMeals = () => {
   const DUMMY_MENU = [
     {
@@ -42,7 +44,7 @@ const AvailableMeals = () => {
   ));
 
   return (
-    <section>
+    <section className={classes.meals}>
       <Card>
         <ul>{mealsList}</ul>
       </Card>
