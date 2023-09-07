ee09a4f5cfdc248dba601299f326e3caf47308c5
Jake Jeong <qpsqps321@gmail.com>
2023-09-07 20:35:48 +0900

    [feat] 모달 작업: 모달창에 있는 버튼들 스타일링.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,21 @@
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 
+import classes from "./Cart.module.css";
+
 const Cart = ({ onCloseModal }) => {
   return (
     <Modal onCloseModal={onCloseModal}>
       <CartItem />
-      <div>
+      <div className={classes.total}>
         <span>Total Amount</span>
         <span>777</span>
       </div>
-      <div>
-        <button onClick={onCloseModal}>Close</button>
-        <button>Order</button>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]} onClick={onCloseModal}>
+          Close
+        </button>
+        <button className={classes.button}>Order</button>
       </div>
     </Modal>
   );
