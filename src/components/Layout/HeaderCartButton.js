a7b1cb0db9cf3ad9a68222232765bcb0013eb2c2
Jake Jeong <55061153+qpsqps123@users.noreply.github.com>
2023-09-10 06:22:19 +0900

    [feat] 헤더 작업: 카트 아이템이 추가/제거될 때마다 카트 버튼이 출렁거리는 애니메이션 추가.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,23 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = ({ onOpenModal }) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.boing : ""
+  }`;
 
   const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button
-      className={`${classes.button} ${classes.bump}`}
-      onClick={onOpenModal}
-    >
+    <button className={btnClasses} onClick={onOpenModal}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
