324becad5a2295522c73a0bdfcd69cb147f5e8c6
Jake Jeong <55061153+qpsqps123@users.noreply.github.com>
2023-09-29 21:20:49 +0900

    [comment] useHttp 훅sendRequest 메서드의 JSDoc 설명 수정.

--- src/firebase/database/use-http.js
+++ src/firebase/database/use-http.js
@@ -9,7 +9,6 @@ const useHttp = () => {
    *
    * @param {string|object} fetchInfo fetchAPI에 인자로 전달할 데이터입니다. 데이터 형식은 객체입니다. 키 구성은 url, method, headers, body입니다. body값은 JSON.stringfy()에 인자로 전달됩니다.
    * @param {function(object)} applyData 응답받은 값을 사용해 추가 로직을 실행하는 콜백함수입니다.
-   * @param {object} applyData.data 통신 성공 시 응답받는 값입니다. applyData 함수의 인자로 전달됩니다.
    */
   const sendRequest = useCallback(async (fetchInfo, applyData) => {
     try {
